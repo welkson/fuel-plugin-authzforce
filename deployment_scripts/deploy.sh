ff33a49334f3d912b2e4dd79973c59bf10cf66d8
Welkson Renny de Medeiros <welkson.medeiros@ifrn.edu.br>
2016-09-22 18:30:21 -0300

    initial commit

---
+++ deployment_scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -eux
+
+# It's a script which deploys your plugin
+echo fuel_plugin_authzforce > /tmp/fuel_plugin_authzforce
+
+OS_NAME=''
+if   grep -i CentOS /etc/issue.net >/dev/null; then
+    OS_NAME='centos';
+elif grep -i Ubuntu /etc/issue.net >/dev/null; then
+    OS_NAME='ubuntu';
+fi
+
+function install_package {
+    if [ $OS_NAME == 'ubuntu' ]; then
+        apt-get install -y --force-yes -o 'APT::Get::AllowUnauthenticated=1' default-jdk tomcat7
+    elif [ $OS_NAME == 'centos' ]; then
+        yum install -y tomcat
+    fi
+}
+
+install_package 
